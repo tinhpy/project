20113f1a090d715e4b7dba234500186c811a2dee
tinh <tinh850256@gmail.com>
2019-07-06 07:02:19 +0700

    update

--- routes/index.js
+++ routes/index.js
@@ -2,8 +2,10 @@ const express = require('express');
 const router = express.Router();
 const { ensureAuthenticated } = require('../config/auth');
 
+
 // Welcome Page
 router.get('/', (req, res) => res.render('welcome'));
+
 
 // Dashboard
 router.get('/dashboard', ensureAuthenticated, (req, res) => {
